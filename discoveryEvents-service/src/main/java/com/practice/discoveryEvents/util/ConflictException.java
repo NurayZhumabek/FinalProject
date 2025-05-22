5760f964aa249dee2fc5780d6406cb8777cd6947
NurayZhumabek <nurazh3004@gmail.com>
2025-05-22 13:36:10 +0500

    upd

---
+++ discoveryEvents-service/src/main/java/com/practice/discoveryEvents/util/ConflictException.java
@@ -0,0 +1,7 @@
+package com.practice.discoveryEvents.util;
+
+public class ConflictException extends RuntimeException {
+    public ConflictException(String message) {
+        super(message);
+    }
+}
